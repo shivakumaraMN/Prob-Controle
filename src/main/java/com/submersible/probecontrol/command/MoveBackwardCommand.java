b3d5a4cd021d1c00c7d6b4f12926baa372bf8be0
Shivakumara <shivu0607@gmail.com>
2026-01-12 19:27:52 +0530

    MoveBackward method implementation

---
+++ src/main/java/com/submersible/probecontrol/command/MoveBackwardCommand.java
@@ -0,0 +1,21 @@
+package com.submersible.probecontrol.command;
+
+import com.submersible.probecontrol.model.Grid;
+import com.submersible.probecontrol.model.Position;
+import com.submersible.probecontrol.model.Probe;
+
+public class MoveBackwardCommand implements Command {
+    @Override
+    public void execute(Probe probe, Grid grid) {
+        Position current = probe.getPosition();
+        Position next = switch (probe.getDirection()) {
+            case NORTH -> current.move(0, -1);
+            case SOUTH -> current.move(0, 1);
+            case EAST -> current.move(-1, 0);
+            case WEST -> current.move(1, 0);
+        };
+        if (grid.canMoveTo(next)) {
+            probe.moveTo(next);
+        }
+    }
+}
