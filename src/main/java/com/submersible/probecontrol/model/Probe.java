5f6366f7da893d6f18b36d6d9fb9274540552b1c
Shivakumara <shivu0607@gmail.com>
2026-01-12 18:05:20 +0530

    Probe model class created

---
+++ src/main/java/com/submersible/probecontrol/model/Probe.java
@@ -0,0 +1,42 @@
+package com.submersible.probecontrol.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Probe {
+    private Position position;
+    private Direction direction;
+    private final List<Position> visitedPositions;
+
+    public Probe(Position initialPosition, Direction initialDirection) {
+        this.position = initialPosition;
+        this.direction = initialDirection;
+        this.visitedPositions = new ArrayList<>();
+        this.visitedPositions.add(initialPosition); // Start is visited
+    }
+
+    public void moveTo(Position newPosition) {
+        this.position = newPosition;
+        this.visitedPositions.add(newPosition);
+    }
+
+    public void turnLeft() {
+        this.direction = direction.turnLeft();
+    }
+
+    public void turnRight() {
+        this.direction = direction.turnRight();
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public List<Position> getVisitedPositions() {
+        return List.copyOf(visitedPositions); // Immutable copy
+    }
+}
