1a1bd58db0b992c6cddb1171e8b710a8fa3afc7c
Shivakumara <shivu0607@gmail.com>
2026-01-13 11:53:26 +0530

    Probe service and controller class

---
+++ src/main/java/com/submersible/probecontrol/controller/ProbeController.java
@@ -0,0 +1,9 @@
+package com.submersible.probecontrol.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/probe")
+public class ProbeController {
+
+}
