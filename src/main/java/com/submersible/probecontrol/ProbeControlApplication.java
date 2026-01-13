dc635826f55ae9a5ab811741e440f4df60c79bbb
Shivakumara <shivu0607@gmail.com>
2026-01-13 12:10:04 +0530

    Bean created for Probe initialization

--- src/main/java/com/submersible/probecontrol/ProbeControlApplication.java
+++ src/main/java/com/submersible/probecontrol/ProbeControlApplication.java
@@ -1,7 +1,16 @@
 package com.submersible.probecontrol;
 
+import com.submersible.probecontrol.model.Direction;
+import com.submersible.probecontrol.model.Grid;
+import com.submersible.probecontrol.model.Position;
+import com.submersible.probecontrol.model.Probe;
+import com.submersible.probecontrol.service.ProbeService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Set;
 
 @SpringBootApplication
 public class ProbeControlApplication {
@@ -10,4 +19,15 @@ public class ProbeControlApplication {
 
 		SpringApplication.run(ProbeControlApplication.class, args);
 	}
+
+	@Bean
+	public Grid grid() {
+		return new Grid(10, 10, Set.of());
+	}
+
+	@Bean
+	public CommandLineRunner initialize(ProbeService probeService) {
+		return args -> probeService.saveProbe("default", new Probe(new Position(0, 0), Direction.NORTH));
+	}
 }
+
