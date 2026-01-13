dba24d5dee61c4c0aaf84d4ce72c686550a351cc
Shivakumara <shivu0607@gmail.com>
2026-01-13 12:04:53 +0530

    Service implement to save probe details

--- src/main/java/com/submersible/probecontrol/service/ProbeManager.java
+++ src/main/java/com/submersible/probecontrol/service/ProbeManager.java
@@ -1,8 +1,19 @@
 package com.submersible.probecontrol.service;
 
+import com.submersible.probecontrol.model.Direction;
+import com.submersible.probecontrol.model.Position;
+import com.submersible.probecontrol.model.Probe;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProbeManager {
+    private final ProbeService probeService;
+    public ProbeManager(ProbeService probeService) {
+        this.probeService = probeService;
+    }
+    public void initializeProbe(String id, Position start, Direction direction) {
+        Probe probe = new Probe(start, direction);
+        probeService.saveProbe(id, probe);
+    }
 
 }
--- src/main/java/com/submersible/probecontrol/service/ProbeService.java
+++ src/main/java/com/submersible/probecontrol/service/ProbeService.java
@@ -1,8 +1,23 @@
 package com.submersible.probecontrol.service;
 
+import com.submersible.probecontrol.model.Grid;
+import com.submersible.probecontrol.model.Probe;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ProbeService {
+    private final Map<String, Probe> probes = new HashMap<>(); // In-memory storage
+    private final Grid grid;
+
+    public ProbeService(Grid grid) {
+        this.grid = grid;
+    }
+
+    public void saveProbe(String id, Probe probe) {
+        probes.put(id, probe);
+    }
 
 }
