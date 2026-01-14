ba06668a1630b904fc7555cccfabb543d4ebbf52
Shivakumara <shivu0607@gmail.com>
2026-01-14 11:47:45 +0530

    Fix the test case error

--- src/test/java/com/submersible/probecontrol/controller/ProbeControllerIntegrationTest.java
+++ src/test/java/com/submersible/probecontrol/controller/ProbeControllerIntegrationTest.java
@@ -2,7 +2,7 @@ package com.submersible.probecontrol.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -11,8 +11,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureWebMvc
+@AutoConfigureMockMvc
 class ProbeControllerIntegrationTest {
+
     @Autowired
     private MockMvc mockMvc;
 
